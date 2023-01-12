dd1e5b3ac8ebc42553d41d7a61d648d9ef0fa875
Tatiana SOBOL <sobol.tatiana13@gmail.com>
2023-01-12 19:21:21 +0200

    Add boostrap

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import BootstrapVue3 from 'bootstrap-vue-3';
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import "./assets/main.css";
 
-createApp(App).mount("#app");
+createApp(App).use(BootstrapVue3).mount("#app");
