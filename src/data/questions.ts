4c86eda891ce1585813df801813ff4e84d61ff0d
Tatiana SOBOL <sobol.tatiana13@gmail.com>
2023-06-03 00:46:54 +0300

    Questions set 2

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -3,164 +3,164 @@ import type { Question } from "@/interfaces/question";
 export const questions: Array<Question> = [
   {
     author: {
-      name: "Dama din Franța",
+      name: "Testul Turing",
+      picture: "/players/turing.png",
     },
     description:
-      'Aceasta "damă"  a apărut in 1889 ca să comemoreze 100 de ani de la revoluția franceză.  Nu a fost demontată doar datorită invenției radioului.',
-    question: "Ce este ea?",
+      "Eu nu mă tem de un computer care trece testul Turing. Eu mă tem de unul care îl pică <b>X</b>",
+    question: "Ce cuvânt a fost înlocuit prin X?",
     answer: {
-      pictures: ["/answers/turnul-eiffel.jpeg"],
+      text: "intenționat",
     },
     active: true,
   },
   {
     author: {
-      name: "Jupiter și Terra",
-      picture: "/players/jupiter-and-terra.jpg",
+      name: "Furtuni de zăpadă",
     },
     description:
-      "Există o teorie printre astronomi care susține că viața pe Pământ nu ar fi posibilă dacă nu ar exista gigantul Jupiter.",
-    question: "Ce zice această teorie?",
+      "Pe timpul furtunilor de zăpadă, în satele rusești făceau asta pentru a-i ajuta pe cei prinși pe câmpuri să găsească satul.",
+    question: "Ce acțiune făceau?",
     answer: {
-      text: "Datorită gravitației sale, Jupiter atrage cea mai mare parte a asteroizilor ce ajung în Sistemul Solar.",
-      pictures: ["/answers/jupiter-asteroids.jpg"],
+      text: "Băteau clopotele",
     },
     active: true,
   },
   {
     author: {
-      name: "Legenda africană",
-      picture: "/players/swahili.jpg",
+      name: "Balto",
+      picture: "/players/balto.jpg",
     },
     description:
-      "Conform legendei poporului african Swahili, primul om pe pământ a venit din cer.",
-    question:
-      "Cine l-a ajutat pe om să coboare din cer conform acestei legende?",
+      "În Parcul Central din New York găsim statuia lui Balto care, împreună cu alți câini, a salvat în 1925 câteva localități de la o epidemie de difterie.",
+    question: "În care stat SUA se aflau aceste localități?",
     answer: {
-      pictures: ["/answers/giraffe.jpg"],
+      text: "Alaska",
     },
     active: true,
   },
   {
     author: {
-      name: "America",
+      name: "Gaz M-20",
+      picture: "/players/gaz-m20.jpg",
     },
     description:
-      'În America a fost tipărită o carte pentru copii cu mesajul "Învață să nu fii rasist cu <b>X</b>. <b>X</b> este alb, negru si asiatic".',
-    question: "Ce a fost înlocuit prin X?",
+      'Inițial modelul gaz M-20 se numea <b>X</b>. După ce Stalin a întrebat "Deci, cu cât vindeți <b>X</b>?", modelul a fost redenumit în Pobeda (Victorie).',
+    question: "Ce a fost înlocuit prin X? (Cum se numea inițial acest model?)",
     answer: {
-      text: "Panda",
-      pictures: ["/answers/panda-book.png"],
+      text: "Patria",
     },
     active: true,
   },
   {
     author: {
-      name: "Japonia",
-      picture: "/players/tsunami-word.png",
+      name: "5kg",
     },
-    description: '<b>X</b> din japoneză se traduce "val mare în port".',
-    question: "Ce a fost înlocuit prin X?",
+    description:
+      "Oamenii de știință au constatat că pe parcursul vieții sale, barbatul consumă în medie aproximativ 5kg din acest produs. " +
+      "În pofida faptului că produsul în sine nu este comestibil, procesul consumării este plăcut și benefic pentru sănătate.",
+    question: "Despre ce produs este vorba?",
     answer: {
-      text: "Tsunami",
-      pictures: ["/answers/tsunami-earthquake.jpg"],
+      text: "Ruj",
+      pictures: ["/answers/lipstick.jpeg"],
     },
     active: true,
   },
   {
     author: {
-      name: "Sacii de făină",
-      picture: "/players/flour-sacs.jpg",
+      name: "Daltoniștii",
     },
     description:
-      "În timpul Marii Depresiuni, companiile care distribuiau făină au observat că oamenii folosesc sacii goi într-un anumit mod. De aceea au început să distribuie făina în saci colorați și cu diferite ornamente.",
-    question: "Cum anume utilizau oamenii sacii goi de făină?",
+      "Contrar opiniei publice, pe ei nu-i deranjează culoarea, întrucât sunt daltoniști, ci mișcările bruște și provocatoare. ",
+    question: "Cine sunt ei?",
     answer: {
-      text: "Făceau haine",
-      pictures: ["/answers/girls-dresses.jpg"],
+      text: "Taurii",
+      pictures: ["/answers/bull.jpg"],
     },
     active: true,
   },
   {
     author: {
-      name: "Grădinile zoologice",
+      name: "Pixurile BIC",
+      picture: "/players/bic.jpg",
     },
     description:
-      "Marile orașe ale lumii de regulă au doar o singură grădină zoologică mare și poate altele mai mici. Acest oraș european are însă două grădini zoologice mari, prima fondată în 1844, a doua în 1955.",
-    question: "Despre ce oraș european merge vorba?",
+      "În anul 1991 compania BIC a făcut o schimbare mimoră în designul pixurilor, schimbare care mai apoi a salvat viața a sute de copii și chiar adulți.",
+    question: "Despre ce modificare este vorba?",
     answer: {
-      text: "Berlin",
-      pictures: ["/answers/zoo-berlin.jpg", "/answers/Tierpark-Berlin.jpg"],
+      text: "Au făcut o gaură în dopul pixurilor.",
+      pictures: ["/answers/bic-pen-hole.jpeg"],
     },
     active: true,
   },
   {
     author: {
-      name: "Corăbiile și ciuma",
-      picture: "/players/italian-ports.jpg",
+      name: "Culmea puterii",
     },
     description:
-      "La începutul perioadei moderne, corăbiile care erau suspectate că erau purtătoare ale ciumei negre erau supuse unei perioade de izolare înainte de a li se permite accesul în porturile italiene.",
-    question: "Câte zile dura această perioadă de izolare?",
+      "Conform unei glume, să tragi cu el este culmea puterii. Cel din Chișinău se află pe strada Ștefan cel Mare, cel din București se află în piața cu același nume," +
+      " iar cel din Paris în Piața Stelei.",
+    question: "Ce este el?",
     answer: {
-      text: "40 zile - carantina",
+      text: "Arc de triumf",
+      pictures: ["/answers/arcul_de_triumf.jpg"],
     },
     active: true,
   },
   {
     author: {
-      name: "Regele leu",
-      picture: "/players/regele-leu.jpeg",
+      name: "Fanii din Napoli",
+      picture: "/players/napoli.jpeg",
     },
     description:
-      'Desenul animat "Regele leu" la început trebuia să se numească "Regele junglei".',
-    question: "De ce totuși producătorii au hotărât să-i schimbe numele?",
+      "În 1987, când echipa de fotbal din Napoli a câștigat pentru prima oară în istorie titlul de campioană a Italiei, bucuria fanilor nu avea margini, iar pe un gard din oraș a apărut inscripția" +
+      ' "Nici nu știți ce ați pierdut."',
+    question: "Ce anume împrejmuia acel gard?",
     answer: {
-      text: "Leul trăiește în savană",
+      text: "Un cimitir",
+      pictures: ["/answers/napoli-cimitery.jpeg"],
     },
     active: true,
   },
   {
     author: {
-      name: "Socrate",
-      picture: "/players/socrate.jpg",
+      name: "Accesoriu",
     },
     description:
-      "Socrate sfătuia bărbații să se însoare, fiindcă dacă vor găsi o soție bună vor fi fericiți, dacă vor găsi una rea vor deveni X.",
-    question: "Ce a fost înlocuit prin X?",
+      "Conform unei versiuni, această expresie românească vine de la un accesoriu pe care îl purtau fetele în zilele de sărbătoare, " +
+      "accesoriu care atunci însemna că viața lor e simplă și lipsită de griji.",
+    question: "Care este această expresie din 3 cuvinte?",
     answer: {
-      text: "Filosofi",
+      text: "Floare la ureche",
+      pictures: ["/answers/floare-la-ureche.png"],
     },
     active: true,
   },
   {
     author: {
-      name: "Volkswagen",
-      picture: "/players/Volkswagen_logo.png",
+      name: "Expresie românească",
+      picture: "/players/colac-peste-pupaza.png",
     },
-    description:
-      'Sloganul acestei publicități, care aparține companiei Volkswagen, spune <i>"Nu te baza pe ceva doar pentru că se potrivește"</i>.',
-    question: "La ce făcea apel compania?",
-    pictures: [
-      "/questions/volkswagen-original-parts.jpg",
-      "/questions/volkswagen-original-parts2.jpg",
-    ],
+    description: "",
+    question: "Ce expresie românească este codificată în această imagine?",
     answer: {
-      text: "Utilizarea pieselor originale pentru reparație",
+      text: "Colac peste pupăză",
+      pictures: ["/answers/colac-peste-pupaza.jpeg"],
     },
     active: true,
   },
+
   {
     author: {
-      name: "Marea Britanie și Republica Moldova",
+      name: "Mitologia greacă",
+      picture: "/players/vertebra-atlas.png",
     },
     description:
-      "În Republica Moldova, X a fost schimbat cu cel curent în 1995. În Marea Britanie, X a suferit o modificare minoră în 1953 și anul trecut.",
-    question: "Ce a fost înlocuit prin X??",
+      "Prima vertebră verticală, cea care susține craniul, poartă numele unui personaj din mitologia greacă.",
+    question: "Care e numele vertebrei?",
     answer: {
-      text:
-        "<b>Imnul de stat</b>.<br><br>" +
-        "În Marea Britanie, imnul se modifică in funție de sexul monarhului.<br><br> Înainte de stabilirea imnului curent, Republica Moldova a avut pentru câțiva ani același imn de stat ca și România, imnul „Deșteaptă-te, române!”. ",
+      text: "Atlas",
+      pictures: ["/answers/atlas.JPG"],
     },
     active: true,
   },
